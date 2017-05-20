3cb33602eabe190287636b1036eac0880f42aa73
ksamualtolch <ksamualtolch@gmail.com>
2017-05-20 12:27:46 -0700

    Initial commit

---
+++ assets/js/lib/googlemap.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 34.023250, lng: -118.402988};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
